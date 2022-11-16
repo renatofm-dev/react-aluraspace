83b7ca447f8ea7240d1ed4016587b056edc62734
Renato Martins <rmartins6694@gmail.com>
2022-11-16 10:29:41 -0300

    add Banner

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,20 @@
+import Banner from '../../components/Banner'
 import Header from '../../components/Header'
 import Menu from '../../components/Menu'
+import styles from './Home.module.scss'
 
 export default function Home (){
     return (
         <>
             <Header></Header>
-            <Menu />
+            <main>
+                <secton className={styles.principal}>
+                    <Menu />
+                    <Banner />  
+
+                </secton>
+            </main>
+            
         </>
 
     )
