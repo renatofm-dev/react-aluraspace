04fb04acb07130e166930d2010c8e4b42a865aea
Renato Martins <rmartins6694@gmail.com>
2022-11-16 09:58:03 -0300

    add icons in Menu

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,12 @@
 import Header from '../../components/Header'
+import Menu from '../../components/Menu'
 
 export default function Home (){
     return (
-        <Header></Header>
+        <>
+            <Header></Header>
+            <Menu />
+        </>
+
     )
 }
