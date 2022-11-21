7db46f7e2ecb66d4c0337453ec03e5d3b09cdd09
Renato Martins <rmartins6694@gmail.com>
2022-11-21 09:58:12 -0300

    add imagens and icons

--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 import Tags from '../Tags'
 import styles from './Galeria.module.scss'
+import fotos from "./fotos.json"
+import open from './open.png'
+import favorito from './favorito.png'
 
 export default function Galeria() {
   return (
     <section className={styles.galeria}>
       <h2>Navegue pela Galeria</h2>
       <Tags/>
-      <ul className={styles.galeria__lista}></ul>
+      <ul className={styles.galeria__cards}>
+        {fotos.map((foto) =>{
+          return(
+            <li key={foto.id} className={styles.galeria__card}>
+              <img 
+              className={styles.galeria__imagem}
+              src={foto.imagem}
+              alt={foto.titulo}
+              />
+              <p className={styles.galeria__descricao}>{foto.titulo}</p>
+              <div>
+                <p>{foto.creditos}</p>
+                <span>
+                  <img src={favorito} alt='icone de curtir' />
+                  <img src={open} alt='icone de modal' />
+                </span>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </section>
   )
 }
